562c7867f188892c03e33b5ae69e12b7376e55fc
om <zawarom15@gmail.com>
2023-11-15 12:01:28 +0530

    added

---
+++ php1.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo"<h2> Hello </h2>";
+    echo"Hello everyone this is my first code in php <br>";
+    echo"om zawar<br>";
+    echo"om","zawar";
+    echo"<br>";
+    echo"om"."zawar";
+    ?>
+    <h1>
+        Hello
+    </h1>
+</body>
+</html>
